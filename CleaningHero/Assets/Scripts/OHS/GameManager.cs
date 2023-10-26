73c71e9605517807a1d701e3344aacd565d439b3
diffth <diffth@nate.com>
2023-10-26 11:31:55 +0900

    MainScene GameManager Update Commit
    1026 1st Commit

--- CleaningHero/Assets/Scripts/OHS/GameManager.cs
+++ CleaningHero/Assets/Scripts/OHS/GameManager.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
     public GameState gState;
+
+    PlayerMove player;
+
+    public GameObject gameOption;
 
     private void Awake()
     {
@@ -29,6 +34,7 @@ public class GameManager : MonoBehaviour
     {
         gState = GameState.Ready;
         StartCoroutine(ReadyToStart());
+        player = GameObject.Find("Player_Dummy").GetComponent<PlayerMove>();
     }
 
     IEnumerator ReadyToStart()
@@ -43,6 +49,50 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.hp <= 10)
+        {
+            //gameLabel.SetActive(true);
+            //gameText.text = "Game Over";
+            //gameText.color = new Color32(255, 0, 0, 255);
+            gState = GameState.GameOver;
+        }
+
+        if (player.hp <= 0)
+        {
+            player.GetComponentInChildren<Animator>().SetFloat("MoveMotion", 0f);
+
+            //gameLabel.SetActive(true);
+            //gameText.text = "Game Over";
+            //gameText.color = new Color32(255, 0, 0, 255);
+            //Transform.buttons = gameText.transform.GetChild(0);
+            //buttons.gameObject.SetActive(true);
+
+            gState = GameState.GameOver;
+        }
+    }
+
+    public void OpenOptionWindow()
+    {
+        gameOption.SetActive(true);
+        Time.timeScale = 0f;
+        gState = GameState.Pause;
+    }
+    public void CloseOptionWindow()
+    {
+        gameOption.SetActive(false);
+        Time.timeScale = 1f;
+        gState = GameState.Run;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
