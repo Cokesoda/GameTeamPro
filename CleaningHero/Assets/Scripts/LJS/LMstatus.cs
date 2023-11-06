df693ea00363a8274c2b965eb21a5750a1f60b3c
wnsgns030 <144082648+wnsgns030@users.noreply.github.com>
2023-11-06 12:28:32 +0900

    hp 카운트 추가
    hp 카운트 추가

--- CleaningHero/Assets/Scripts/LJS/LMstatus.cs
+++ CleaningHero/Assets/Scripts/LJS/LMstatus.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LMstatus : MonoBehaviour
 {
-    public float playerHp = 100;
+    public float playerHp = 10;
 
     public float playerAttackDistance = 10;//플레이어 공격 거리
     public float playerAttackDamage = 10;  //플레이어 공격 데미지
